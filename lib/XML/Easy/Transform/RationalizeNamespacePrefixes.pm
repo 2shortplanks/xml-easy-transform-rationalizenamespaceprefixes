5f282d10225f6a0e1fa09776dfb23c6cdc2ae286
2shortplanks <mark@twoshortplanks.com>
2009-05-15 14:54:23 +0100

    initial add

---
+++ lib/XML/Easy/Transform/RationalizeNamespacePrefixes.pm
@@ -0,0 +1,251 @@
+package XML::Easy::Transform::RationalizeNamespacePrefixes;
+use base qw(Exporter);
+
+our @EXPORT;
+
+use strict;
+use warnings;
+
+=head1 SYNOPSIS
+
+  use XML::Easy::Transform::RationalizeNamespacePrefixes qw(
+     rationalize_namespace_prefixes
+  );
+  
+  my $doc = rationalize_namespace_prefixes(
+     xml10_read_document($text)
+  );
+
+=head1 DESCRIPTION
+
+This code creates a new tree of XML::Easy::Element nodes by examining
+an existing XML::Easy::Element tree and producing a new tree that is
+schemantically identical under the XML Namespaces 1.0 specification
+but with all namespace declartions moved to the top node of the tree
+(this may involve renaming several elements in the tree to have different
+prefixes.)
+
+For example:
+
+  sub process($) {
+    return xml10_write_document(
+      rationalize_namespace_prefixes(
+        xml10_read_document( $_[0] )
+      ),"UTF-8"
+    );
+  }
+  
+  print process <<'XML';
+  <foo>
+    <ex1:bar xmlns:ex1="http://www.twoshortplanks.com/namespace/example/1"/>
+  </foo>
+  XML
+
+Moves the namespace up and prints:
+
+  <foo xmlns:ex1="http://www.twoshortplanks.com/namespace/example/1">
+    <ex1:bar/>
+  </foo>
+
+The routine will also create prefixes as needed:
+
+  print process <<'XML';
+  <foo>
+    <bar xmlns="http://www.twoshortplanks.com/namespace/example/1"/>
+  </foo>
+  XML
+
+Prints
+
+  <foo xmlns:default2="http://www.twoshortplanks.com/namespace/example/1">
+    <default2:bar/> 
+  </foo>
+
+It even copes with conflicting prefixes:
+
+  print process <<'XML';
+  <muppet:kermit xmlns:muppet="http://www.twoshortplanks.com/namespace/example/muppetshow">
+    <muppet:kermit xmlns:muppet="http://www.twoshortplanks.com/namespace/example/seasmestreet" />
+  </muppet:kermit>
+  XML
+
+Prints
+
+  <muppet:kermit xmlns:muppet="http://www.twoshortplanks.com/namespace/example/muppetshow" xmlns:muppet2="http://www.twoshortplanks.com/namespace/example/seasmestreet">
+    <muppet2:kermit/>
+  </muppet:kermit>
+
+This module also removes all unnecessary prefixes on attributes:
+
+  <wobble xmlns:ex1="http://www.twoshortplanks.com/namespace/example/1">
+    <ex1:wibble ex1:jelly="in my tummy"/>
+  </wobble>
+
+Will be transformed into
+
+  <wobble xmlns:ex1="http://www.twoshortplanks.com/namespace/example/1">
+    <ex1:wibble jelly="in my tummy"/>
+  </wobble>
+
+=cut
+
+{
+  our $prefix_generator;
+
+
+  my %default_known_prefixes = (
+    # include these as we're not meant to freak out if these namespaces are used
+    xml => "http://www.w3.org/XML/1998/namespace",
+    xmlns => "http://www.w3.org/2000/xmlns/",
+    
+    # by default the empty string is bound to ""
+    "" => "",
+  );
+
+  # this holds the namespaces that we've assigned.
+  my %assigned_prefixes;
+  my %assigned_ns;
+
+  sub rationalize_namespace_prefixes ($;&) {
+    my $source_element = shift;
+    local $prefix_generator = shift || \&_prefix_generator;
+
+    # create the modified tree and populate our two local hashes with
+    # the namespaces we should have
+
+    %assigned_prefixes = ();
+    %assigned_ns = ();
+    my $dest_element = _rnp($source_element, \%default_known_prefixes);
+    
+    # we now have a tree with *no* namespaces.  Replace the top of that
+    # tree with a new element that is the same as the top element of the tree but
+    # with the needed namespace declarations
+    
+    my $attr = +{ %{ $dest_element->attributes }, map {
+      ($_ ne "") ? ("xmlns:$_" => $assigned_prefixes{$_}) :
+        ($assigned_prefixes{""} ne "") ? ( xmlns => $assigned_prefixes{""} ) : ()
+    } keys %assigned_prefixes };
+    
+    return XML::Easy::Element->new($dest_element->type_name, $attr, $dest_element->content_object);
+  }
+  push @EXPORT, "rationalize_namespace_prefixes";
+
+  sub _rnp {
+    my $element = shift;
+
+    # what namespaces are in scope at the momement
+    my $known_prefixes = shift;
+
+    # boolean that indicates if known_* is our copy or the
+    # version passed in (has it been copy-on-write-ed)
+    my $cowed = 0;
+    
+    # change the name of the element
+    my $attr = $element->attributes;
+    foreach (sort keys %{ $attr }) {
+      next unless /^xmlns(?::(.*))?$/;
+      my $prefix = defined $1 ? $1 : "";
+      my $ns     = $attr->{$_};
+
+      # check for things assigning namespaces to reserved places
+      die "Specification violation: Can't assign '$ns' to prefix 'xml'"
+        if $prefix eq "xml" && $ns ne 'http://www.w3.org/XML/1998/namespace';
+      die "Specification violation: Can't assign 'http://www.w3.org/2000/xmlns/' to any prefix"
+        if $ns eq 'http://www.w3.org/2000/xmlns/';
+
+      # check we're not assigning things to the xmlns prefix
+      die "Specification violation: Can't assign any namespace to prefix 'xmlns'"
+        if $prefix eq 'xmlns';
+
+      # copy the hash if we haven't done so already
+      unless ($cowed) {
+        $known_prefixes = +{ %{ $known_prefixes } };
+        $cowed = 1;
+      }
+
+      # record that this prefix maps to this namespace;
+      $known_prefixes->{ $prefix } = $ns;
+
+      unless ($assigned_ns{ $ns }) {
+        # find an unused unique prefix in the destination.
+        while (exists $assigned_prefixes{ $prefix }) {
+          $prefix = $prefix_generator->($prefix);
+        }
+
+        # remember that we're mapping that way
+        $assigned_prefixes{ $prefix } = $ns;
+        $assigned_ns{ $ns } = $prefix;
+      }
+
+    }
+
+    # munge the prefix on the main element
+    $element->type_name =~ /^([^:]+)(?::(.*))?$/
+      or die "Invalid element name '".$element->type_name."'";
+    my $prefix     = defined ($2) ? $1 : "";
+    my $local_name = defined ($2) ? $2 : $1;
+
+    # map the prefix in the source document to a namespace,
+    # then look up the corrisponding prefix in the destination document
+    my $element_ns;
+    my $new_element_prefix;
+    if ($prefix eq "" && !exists($assigned_prefixes{""})) {
+      # someone just used the default (empty) prefix for the first time without having
+      # declared an explict namespace.  Remember that the empty namespace exists.
+      $element_ns = $assigned_prefixes{""} = "";
+      $new_element_prefix = $assigned_ns{""} = "";
+    } else {
+      $element_ns = $known_prefixes->{ $prefix };
+      unless (defined $element_ns) { die "Prefix '$prefix' has no registered namespace" }
+      $new_element_prefix = $assigned_ns{ $element_ns };
+    }
+    my $new_element_name = (length $new_element_prefix) ? "$new_element_prefix:$local_name" : $local_name;
+
+    # munge the prefix on the attribute elements
+    my $new_attr = {};
+    foreach (keys %{ $attr }) {
+      /^([^:]+)(?::(.*))?$/
+        or die "Invalid attribute name '$_'";
+      my $prefix     = defined ($2) ? $1 : "";
+      my $local_name = defined ($2) ? $2 : $1;
+      
+      # skip the namespaces
+      next if $prefix eq "" && $local_name eq "xmlns";
+      next if $prefix eq "xmlns";
+      
+      # map the prefix in the source document to a namespace,
+      # then look up the corrisponding prefix in the destination document
+      my $ns = $prefix eq "" ? $element_ns : $known_prefixes->{ $prefix };
+      unless (defined $ns) { die "Prefix '$prefix' has no registered namespace" }
+      my $new_prefix = $assigned_ns{ $ns };
+      
+      my $final_name = ($new_prefix ne $new_element_prefix) ? "$new_prefix:$local_name" : $local_name;
+      $new_attr->{ $final_name } = $attr->{ $_ };
+      
+    }
+    
+    my @content = @{ $element->content };
+    my @new_content;
+    while (@content) {
+      push @new_content, shift @content;
+      if (@content) {
+        push @new_content, _rnp((shift @content), $known_prefixes);
+      }
+    }
+    
+    return XML::Easy::Element->new( $new_element_name, $new_attr, \@new_content );
+  }
+}
+
+sub _prefix_generator {
+  my $prefix = shift;
+
+  # "" => default2 (the 2 is concatinated later)
+  $prefix = "default" if $prefix eq "";
+
+  # turn foo into foo2 and foo2 into foo3, etc.
+  $prefix .= "2" unless $prefix =~ s/(\d+)$/ $1 + 1 /e;
+
+  return $prefix;
+}
+
---
+++ t/01rnpdoku.t
@@ -0,0 +1,166 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 13;
+
+use XML::Easy::Text qw(xml10_read_document xml10_write_element);
+use XML::Easy::Transform::RationalizeNamespacePrefixes qw(
+   rationalize_namespace_prefixes
+);
+
+sub process($) {
+  return xml10_write_element(
+    rationalize_namespace_prefixes(
+      xml10_read_document( $_[0] )
+    ),
+  );
+}
+
+sub chompp($) {
+  my $thingy = shift;
+  chomp $thingy;
+  return $thingy;
+}
+
+is process <<'XML', chompp <<'XML', "move up";
+<foo>
+  <ex1:bar xmlns:ex1="http://www.photobox.com/namespace/example1" />
+</foo>
+XML
+<foo xmlns:ex1="http://www.photobox.com/namespace/example1">
+  <ex1:bar/>
+</foo>
+XML
+
+is process <<'XML', chompp <<'XML', "default";
+<foo>
+  <bar xmlns="http://www.photobox.com/namespace/example1">
+    <bazz zing="zang">
+      <buzz xmlns="http://www.photobox.com/namespace/example2"/>
+    </bazz>
+  </bar>
+</foo>
+XML
+<foo xmlns:default2="http://www.photobox.com/namespace/example1" xmlns:default3="http://www.photobox.com/namespace/example2">
+  <default2:bar>
+    <default2:bazz zing="zang">
+      <default3:buzz/>
+    </default2:bazz>
+  </default2:bar>
+</foo>
+XML
+
+is process <<'XML', chompp <<'XML', "muppet";
+<muppet:kermit xmlns:muppet="http://www.photobox.com/namespace/example/muppetshow" >
+  <muppet:kermit xmlns:muppet="http://www.photobox.com/namespace/example/seasmestreet"/>
+</muppet:kermit>
+XML
+<muppet:kermit xmlns:muppet="http://www.photobox.com/namespace/example/muppetshow" xmlns:muppet2="http://www.photobox.com/namespace/example/seasmestreet">
+  <muppet2:kermit/>
+</muppet:kermit>
+XML
+
+is process <<'XML', chompp <<'XML', "lost attribute prefix";
+<wobble xmlns:ex1="http://www.twoshortplanks.com/namespace/example/1" xmlns:ex1also="http://www.twoshortplanks.com/namespace/example/1">
+  <ex1:wibble ex1:jelly="in my tummy" ex1also:yum="yum yum"/>
+</wobble>
+XML
+<wobble xmlns:ex1="http://www.twoshortplanks.com/namespace/example/1">
+  <ex1:wibble jelly="in my tummy" yum="yum yum"/>
+</wobble>
+XML
+
+is process <<'XML', chompp <<'XML', "no prefix in on attribute in src";
+<a xmlns:ex1="http://www.twoshortplanks.com/namespaces/example/1" xmlns:ex1also="http://www.twoshortplanks.com/namespaces/example/1">
+  <ex1:b local="for local people" ex1also:alsolocal="as well"/>
+</a>
+XML
+<a xmlns:ex1="http://www.twoshortplanks.com/namespaces/example/1">
+  <ex1:b alsolocal="as well" local="for local people"/>
+</a>
+XML
+
+
+is process <<'XML', chompp <<'XML', "no default till later";
+<ex1:a xmlns:ex1="http://www.twoshortplanks.com/namespaces/example/1">
+  <b xmlns="http://www.twoshortplanks.com/namespaces/example/2"/>
+</ex1:a>
+XML
+<ex1:a xmlns="http://www.twoshortplanks.com/namespaces/example/2" xmlns:ex1="http://www.twoshortplanks.com/namespaces/example/1">
+  <b/>
+</ex1:a>
+XML
+
+is process <<'XML', chompp <<'XML', "multiple prefixes => 1 prefix";
+<a>
+  <ex3:c xmlns:ex3="http://www.twoshortplanks.com/namespaces/example/3">
+     <ex3also:c xmlns:ex3also="http://www.twoshortplanks.com/namespaces/example/3"/>
+  </ex3:c>
+  <ex3alsoalso:c xmlns:ex3alsoalso="http://www.twoshortplanks.com/namespaces/example/3"/>
+</a>
+XML
+<a xmlns:ex3="http://www.twoshortplanks.com/namespaces/example/3">
+  <ex3:c>
+     <ex3:c/>
+  </ex3:c>
+  <ex3:c/>
+</a>
+XML
+
+is process <<'XML', chompp <<'XML', "multiple overloaded prefixes";
+<a>
+  <ns:b xmlns:ns="http://www.twoshortplanks.com/namespaces/example/1">
+     <ns:c xmlns:ns="http://www.twoshortplanks.com/namespaces/example/2">
+       <ns:d />
+     </ns:c>
+  </ns:b>
+  <ns:e xmlns:ns="http://www.twoshortplanks.com/namespaces/example/3"/>
+</a>
+XML
+<a xmlns:ns="http://www.twoshortplanks.com/namespaces/example/1" xmlns:ns2="http://www.twoshortplanks.com/namespaces/example/2" xmlns:ns3="http://www.twoshortplanks.com/namespaces/example/3">
+  <ns:b>
+     <ns2:c>
+       <ns2:d/>
+     </ns2:c>
+  </ns:b>
+  <ns3:e/>
+</a>
+XML
+
+eval {
+  process <<'XML'
+<bar xmlns:xmlns="something else"/>
+XML
+};
+like($@, qr/^Specification violation: Can't assign any namespace to prefix 'xmlns'/, "bang - xmlns prefix");
+
+eval {
+  process <<'XML'
+<bar xmlns:xml="something else"/>
+XML
+};
+like($@, qr/^Specification violation: Can't assign 'something else' to prefix 'xml'/, "bang - xml prefix");
+
+eval {
+  process <<'XML'
+<bar xmlns:something="http://www.w3.org/2000/xmlns/"/>
+XML
+};
+like($@, qr{^Specification violation: Can't assign 'http://www.w3.org/2000/xmlns/' to any prefix}, "bang - xmlns namespace");
+
+
+eval {
+  process <<'XML'
+<bar xmlns:xml="http://www.w3.org/XML/1998/namespace"/>
+XML
+};
+ok(!$@, "no bang - xml ns for xml prefix");
+
+eval {
+  process <<'XML'
+<foo:bar />
+XML
+};
+like($@, qr/^Prefix 'foo' has no registered namespace/, "bang - not reg");
